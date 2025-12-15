dcfe19cbc726b570bbe1ff57190f3b8ee8b0873d
neominal.solutions <neominal.solutions@gmail.com>
2025-12-15 11:57:35 +0300

    Initial commit

---
+++ src/main/java/com/tcell_spring/restservice/RestserviceApplication.java
@@ -0,0 +1,83 @@
+package com.tcell_spring.restservice;
+
+import com.tcell_spring.restservice.service.UserService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@Slf4j
+public class RestserviceApplication {
+
+
+	public static void main(String[] args) {
+
+	    ApplicationContext context = SpringApplication.run(RestserviceApplication.class, args);
+		String myBean = context.getBean("myBean", String.class);
+		log.info(myBean);
+		// spring context bean retrieval
+		User user  = context.getBean("userInfo", User.class);
+		log.info(user.getName());
+
+		// Stereotype.Service ile işaretlenmiş UserService bean'ini al ve kullan
+//		UserService userService = context.getBean(UserService.class);
+//		log.info(userService.getUserInfo());
+
+
+		// DI olmasaydı Spring Context bunu yönetmeseydi
+		UserRepository userRepository = new UserRepository();
+		UserService userService2 = new UserService(userRepository);
+		userService2.getUserInfo();
+		log.info(userService2.getUserInfo());
+
+		// Son Senaryo ise 1 adet interface üzerinden bu interfaceden implemente olan birden fazla class varsa nasıl bir spring context yönetimi olur ? @Qualifier ile ayırt edebiliriz. @Primary ile birini default yapabiliriz.
+
+
+
+		// Bir servis içerisindeki Dependecy kısmını DI ile nasıl alıcağız.
+
+	}
+
+	// @Bean annotation to define a bean, register spring container
+	@Bean
+	public String myBean() {
+	    return "testing bean";
+	}
+
+	// Class instanceları proje başlatıldığında oluşturulur ve Spring konteynerine eklenir. Daha sonra bu bean'ler, uygulamanın herhangi bir yerinde ihtiyaç duyulduğunda Spring konteynerinden alınabilir ve kullanılabilir.
+	// Bu, bağımlılık enjeksiyonu (dependency injection) olarak bilinen bir tasarım desenini destekler ve uygulamanın modülerliğini, test edilebilirliğini ve bakımını artırır.
+	@Bean
+	public User userInfo() {
+		User user = new User();
+		user.setName("John Doe");
+		return user;
+	}
+
+	// bu beanlerin uygulamadaki yaşam döngüsü, kapsamı (scope) ve diğer özellikleri de @Bean anotasyonu ile yapılandırılabilir.
+	// Örneğin, bir bean'in singleton (varsayılan) veya prototype scope'ta sahip olup olmayacağını belirleyebilirsiniz.
+	public class User{
+
+		@PostConstruct
+		public void init() {
+			log.info("User bean is created");
+		}
+
+		@PreDestroy // cleanup işlemleri için kullanılır
+		public void destroy() {
+			log.info("User bean is destroyed");
+		}
+
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+}
