49a8a6785581c9fab394099840700aaaadf14b95
neominal.solutions <neominal.solutions@gmail.com>
2025-12-17 10:03:01 +0300

    Add JPQL query methods to ICommentRepository and IPostRepository for fetching comments by post ID and released posts

--- src/main/java/com/tcell_spring/restservice/repository/ICommentRepository.java
+++ src/main/java/com/tcell_spring/restservice/repository/ICommentRepository.java
@@ -1,6 +1,7 @@
 package com.tcell_spring.restservice.repository;
 
 import com.tcell_spring.restservice.entity.Comment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,10 @@ public interface ICommentRepository extends CrudRepository<Comment, Integer> {
     // extend etmek zorunda kaldık. Query methodları kullanabilmek için isimlendirme kurallarına uymalıyız.
     // findBy + fieldName
     List<Comment> findByPostId(Integer postId);
+
+    // Join fetch örneği
+    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.post WHERE c.postId = :postId")
+    List<Comment> findCommentsByPostId(Integer postId);
 
 }
 
--- src/main/java/com/tcell_spring/restservice/repository/IPostRepository.java
+++ src/main/java/com/tcell_spring/restservice/repository/IPostRepository.java
@@ -4,6 +4,7 @@ import com.tcell_spring.restservice.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,6 +12,14 @@ public interface IPostRepository extends JpaRepository<Post, Integer> {
 
     // title üzerinden arama yapabilmek için query method, aynı zamanda sayfalama desteği de ekledik
     Page<Post> findAllByTitleContainsIgnoreCase(String title, Pageable pageable);
+
+    // JPQL kullanarak released olan ve releaseDate'i şu anki zamandan küçük veya eşit olan postları getiren method
+    // Sayfalama işlemleri query methodlarında olduğu gibi Pageable ile yapılır.
+    @Query("SELECT p FROM Post p WHERE p.isReleased = true AND p.releaseDate <= CURRENT_TIMESTAMP")
+    Page<Post> findAllReleasedPosts(Pageable pageable);
+
+
+
 
 }
 
